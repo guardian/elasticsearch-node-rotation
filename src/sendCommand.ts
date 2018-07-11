c01d8a2a9560c0876152b782534fc007359962c7
tomrf1 <tom.forbes@theguardian.com>
2018-07-11 10:52:26 +0100

    sendCommand

--- src/sendCommand.ts
+++ src/sendCommand.ts
@@ -1,27 +1,56 @@
 const AWS = require('aws-sdk');
 
+const ssm = new AWS.SSM();
+
 export function handler(event, context, callback) {
-    const ssm = new AWS.SSM();
+    sendCommand(event.command, event.instanceId)
+        .then(commandId => getCommandResult(commandId, event.instanceId))
+        .then(result => callback(null, result))
+        .catch(error => {
+            console.log("Error: " + error);
+            callback(error)
+        })
+}
 
-    const sendCommandParams = {
-        DocumentName: "AWS-RunShellScript",
-        Parameters: {
-            commands: [
-                event.command
-            ]
-        },
-        InstanceIds: [
-            event.instanceId
-        ]
-    };
-
-    ssm.sendCommand(sendCommandParams,  function (err, data) {
-        if (err) console.log(err, err.stack);
-        else {
-            callback(null, JSON.stringify({
-                commandId: data.Command.CommandId,
-                instanceId: event.instanceId
-            }));
-        }
+function wait(millis) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => resolve(), millis)
     });
+}
+
+function sendCommand(command, instanceId) {
+    return new Promise((resolve, reject) => {
+        const sendCommandParams = {
+            DocumentName: "AWS-RunShellScript",
+            Parameters: {
+                commands: [
+                    command
+                ]
+            },
+            InstanceIds: [
+                instanceId
+            ]
+        };
+
+        ssm.sendCommand(sendCommandParams,function (err, data) {
+            if (err) reject(err);
+            else resolve(data.Command.CommandId)
+        });
+    })
+}
+
+async function getCommandResult(commandId, instanceId) {
+    await wait(2000);
+
+    return new Promise((resolve, reject) => {
+        const getCommandParams = {
+            InstanceId: instanceId,
+            CommandId: commandId
+        };
+
+        ssm.getCommandInvocation(getCommandParams, function (err, data) {
+            if (data.Status === "Success") resolve(data.StandardOutputContent);
+            else reject(data.StatusDetails)
+        });
+    })
 }
