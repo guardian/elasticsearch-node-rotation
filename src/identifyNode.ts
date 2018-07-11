db35f73c4fcce36bfecc168307356fedd67c9317
Jacob Winch <jacob.winch@guardian.co.uk>
2018-07-11 13:47:47 +0100

    Use promises instead of passing around callbacks

--- src/identifyNode.ts
+++ src/identifyNode.ts
@@ -3,40 +3,49 @@ let AWS = require('aws-sdk');
 export function handler(event, context, callback) {
     let asg: string = process.env.ASG_NAME;
     console.log(`Searching for oldest node in ${asg}`);
-    identifyOldestInstance(asg, callback);
+    getInstances(asg)
+        .then(ids => getOldestInstanceId(ids))
+        .then(oldestInstance => callback(null, { "oldestInstance": oldestInstance.id}))
+        .catch(error => callback(error))
+
 }
 
 let awsAutoscaling = new AWS.AutoScaling();
 let awsEc2 = new AWS.EC2();
 
-function identifyOldestInstance(asgName: string, callback) {
+function getInstances(asgName: string): Promise<string[]> {
     let params = { AutoScalingGroupNames: [ asgName ] };
-    awsAutoscaling.describeAutoScalingGroups(params, function(error, data) {
-       if (error) {
-           console.log(error, error.stack, error.statusCode);
-       } else {
-           let instances = data.AutoScalingGroups[0].Instances;
-           let ids = instances.map(instance => instance.InstanceId);
-           getOldestInstanceId(ids, callback);
-       }
-    })
+    let requestPromise = awsAutoscaling.describeAutoScalingGroups(params).promise();
+    return requestPromise.then(
+        function (data) {
+            let instances = data.AutoScalingGroups[0].Instances;
+            return instances.map(instance => instance.InstanceId);
+        },
+        function (error) {
+            console.log(error, error.stack, error.statusCode);
+            return error;
+        }
+    )
 }
 
-function getOldestInstanceId(instanceIds: string[], callback) {
+function getOldestInstanceId(instanceIds: string[]): Promise<Instance> {
     console.log(`Fetching details for: ${instanceIds}`);
     let params = { InstanceIds: instanceIds };
-    awsEc2.describeInstances(params, function (error, data) {
-      if (error) {
-          console.log(error, error.stack, error.statusCode)
-      } else {
-          let instanceArrays = data.Reservations.map(instanceArrays => instanceArrays.Instances);
-          let instances: Instance[] = instanceArrays.concat.apply([], instanceArrays).map(instance => new Instance(instance.InstanceId, instance.LaunchTime));
-          let sortedInstances: Instance[] = instances.sort(function(a,b){return a.launchTime.getTime() - b.launchTime.getTime()});
-          let oldestInstance: Instance = sortedInstances[0];
-          console.log(`Oldest instance ${oldestInstance.id} was launched at ${oldestInstance.launchTime}`);
-          callback(null, { "oldestInstance": oldestInstance.id})
-      }
-    });
+    let requestPromise = awsEc2.describeInstances(params).promise();
+    return requestPromise.then(
+        function(data) {
+            let instanceArrays = data.Reservations.map(instanceArrays => instanceArrays.Instances);
+            let instances: Instance[] = instanceArrays.concat.apply([], instanceArrays).map(instance => new Instance(instance.InstanceId, instance.LaunchTime));
+            let sortedInstances: Instance[] = instances.sort(function(a,b){return a.launchTime.getTime() - b.launchTime.getTime()});
+            let oldestInstance: Instance = sortedInstances[0];
+            console.log(`Oldest instance ${oldestInstance.id} was launched at ${oldestInstance.launchTime}`);
+            return oldestInstance;
+        },
+        function (error) {
+            console.log(error, error.stack, error.statusCode);
+            return error;
+        }
+    )
 }
 
 class Instance {
