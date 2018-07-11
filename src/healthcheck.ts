e8232532f6618a332af98dc2668e9b9c2cbc9399
tomrf1 <tom.forbes@theguardian.com>
2018-07-11 14:16:37 +0100

    revert other changes

--- src/healthcheck.ts
+++ src/healthcheck.ts
@@ -1,43 +1,4 @@
-const AWS = require('aws-sdk');
-
 export function handler(event, context, callback) {
-
-    function wait() {
-      return new Promise((resolve, reject) => {
-        setTimeout(() => resolve(), 2000)
-      });
-    }
-
-    const ssm = new AWS.SSM();
-
-    const sendCommandParams = {
-        DocumentName: "AWS-RunShellScript",
-        Parameters: {
-            commands: [
-                event.command
-            ]
-        },
-        InstanceIds: [
-            event.instanceId
-        ]
-    };
-
-    ssm.sendCommand(sendCommandParams, async function (err, data) {
-        if (err) console.log(err, err.stack);
-        else {
-            await wait();
-
-            const getCommandParams = {
-              InstanceId: event.instanceId,
-              CommandId: data.Command.CommandId
-            };
-
-            ssm.getCommandInvocation(getCommandParams, function(err, data) {
-                if (data.Status === "Success") {
-                    callback(null, data.StandardOutputContent);
-                }
-                else callback(data.StatusDetails, null)
-            });
-        }
-    });
+    console.log("Checking ES cluster health");
+    callback(null, "Success");
 }
