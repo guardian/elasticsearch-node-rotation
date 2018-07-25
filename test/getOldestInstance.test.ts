2bdef7592d8ffcd5980530ba794faf745fd30ba4
Jacob Winch <jacob.winch@guardian.co.uk>
2018-07-25 12:47:19 +0100

    Rename function

--- test/getOldestInstance.test.ts
+++ test/getOldestInstance.test.ts
@@ -1,6 +1,6 @@
 import {findOldestInstance, Instance} from '../src/getOldestInstance'
 
-function addDays(date: Date, days: number): Date {
+function createDate(date: Date, days: number): Date {
     let result = new Date(date);
     result.setDate(result.getDate() + days);
     return result;
@@ -10,8 +10,8 @@ describe("findOldestInstance", () => {
     it("should identify the oldest instance correctly", () => {
         const referenceDate: Date = new Date(2018, 7, 21);
         const oldestInstance = new Instance("id123", referenceDate);
-        const newerInstance = new Instance("id124", addDays(referenceDate, 7));
-        const newestInstance = new Instance("id125", addDays(referenceDate, 21));
+        const newerInstance = new Instance("id124", createDate(referenceDate, 7));
+        const newestInstance = new Instance("id125", createDate(referenceDate, 21));
         const instances: Instance[] = [newerInstance, oldestInstance, newestInstance];
         expect(findOldestInstance(instances)).toEqual(oldestInstance)
     });
