e9be2f3553f8c3158ec9ba161ea0b403803ae4e2
Jacob Winch <jacob.winch@guardian.co.uk>
2018-08-31 11:42:47 +0100

    Clear exclusion list

--- src/removeNode.ts
+++ src/removeNode.ts
@@ -2,6 +2,7 @@ import {OldAndNewNodeResponse} from './utils/handlerResponses';
 import {ssmCommand} from './utils/ssmCommand';
 import {terminateInstance} from "./aws/ec2Instances";
 import {Instance} from './aws/types';
+import {excludeFromAllocation} from "./elasticsearch/elasticsearch";
 
 export async function handler(event: OldAndNewNodeResponse): Promise<OldAndNewNodeResponse> {
 
@@ -10,7 +11,7 @@ export async function handler(event: OldAndNewNodeResponse): Promise<OldAndNewNo
     return new Promise<OldAndNewNodeResponse>((resolve, reject) => {
         ssmCommand("systemctl stop elasticsearch", oldestInstance.id, false)
                 .then(() => terminateInstance(oldestInstance))
-                // TODO clear ES allocation exclusion list here
+                .then(() => excludeFromAllocation("", oldestInstance.id)) // Don't exclude any ips
                 .then(() => resolve(event))
                 .catch(error => {
                     const message = `Failed due to terminate ${oldestInstance.id} due to: ${error}`;
