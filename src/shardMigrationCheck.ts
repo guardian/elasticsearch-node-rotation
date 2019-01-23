3be84b0c1e6211dbce204fb0a72b9dd7e7803893
Roberto Tyley <roberto.tyley@theguardian.com>
2019-01-23 14:12:31 +0000

    Fix clunky variable name...
    https://github.com/guardian/elasticsearch-node-rotation/pull/33#discussion_r250196209

--- src/shardMigrationCheck.ts
+++ src/shardMigrationCheck.ts
@@ -8,11 +8,11 @@ export async function handler(event: OldAndNewNodeResponse): Promise<OldAndNewNo
     ]).then(([clusterStatus, documents]) => {
         const hasDocuments = documents.count > 0;
         const isMasterAndShardsRelocating = event.oldestElasticsearchNode.isMasterEligible && clusterStatus.relocating_shards > 0;
-        const isClusterNotGreen = !(clusterStatus.status === "green");
+        const clusterIsUnhealthy = !(clusterStatus.status === "green");
 
-        if (isClusterNotGreen || hasDocuments || isMasterAndShardsRelocating) {
+        if (clusterIsUnhealthy || hasDocuments || isMasterAndShardsRelocating) {
             const error =
-                `Check failed: hasDocuments=${hasDocuments} isMasterAndShardsRelocating=${isMasterAndShardsRelocating} isClusterNotGreen=${isClusterNotGreen}`;
+                `Check failed: hasDocuments=${hasDocuments} isMasterAndShardsRelocating=${isMasterAndShardsRelocating} clusterIsUnhealthy=${clusterIsUnhealthy}`;
             console.log(error);
             throw new Error(error);
         } else return event;
