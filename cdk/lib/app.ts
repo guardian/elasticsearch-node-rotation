aa983c6adce88871b5be8990197b06f2f9a5ca2e
Jacob Winch <jacob.winch@guardian.co.uk>
2022-06-17 13:43:15 +0100

    Fix linting errors

--- cdk/lib/app.ts
+++ cdk/lib/app.ts
@@ -1,12 +1,12 @@
+import type { GuStackProps } from '@guardian/cdk/lib/constructs/core';
 import { App } from 'aws-cdk-lib';
 import { ElasticsearchNodeRotation } from './stack';
-import {GuStackProps} from "@guardian/cdk/lib/constructs/core";
 
 const app = new App();
 
 export const props: GuStackProps = {
-	stack: "elasticsearch-node-rotation",
-	stage: "INFRA",
-}
+	stack: 'elasticsearch-node-rotation',
+	stage: 'INFRA',
+};
 
-new ElasticsearchNodeRotation(app, "Elasticsearch-Node-Rotation", props);
+new ElasticsearchNodeRotation(app, 'Elasticsearch-Node-Rotation', props);
--- cdk/lib/stack.test.ts
+++ cdk/lib/stack.test.ts
@@ -1,12 +1,16 @@
 import { App } from 'aws-cdk-lib';
 import { Template } from 'aws-cdk-lib/assertions';
+import { props } from './app';
 import { ElasticsearchNodeRotation } from './stack';
-import {props} from "./app";
 
 describe('The generated stack', () => {
 	it('matches the snapshot', () => {
 		const app = new App();
-		const stack = new ElasticsearchNodeRotation(app, "Elasticsearch-Node-Rotation", props);
+		const stack = new ElasticsearchNodeRotation(
+			app,
+			'Elasticsearch-Node-Rotation',
+			props,
+		);
 		const template = Template.fromStack(stack);
 		expect(template.toJSON()).toMatchSnapshot();
 	});
