6125fbb5dc097cf641d67842136304f11ed42433
Jacob Winch <jacob.winch@guardian.co.uk>
2022-06-21 10:33:38 +0100

    Use the latest version of @guardian/cdk-app-ts

--- cdk/.projenrc.js
+++ cdk/.projenrc.js
@@ -1,4 +1,6 @@
 const { GuCDKTypescriptProject } = require("@guardian/cdk-app-ts");
+const { DependencyType } = require("projen");
 const project = new GuCDKTypescriptProject({
 });
+project.deps.addDependency("@guardian/cdk-app-ts@0.1.9", DependencyType.BUILD)
 project.synth();
